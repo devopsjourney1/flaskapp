d5b8d8589d2c4b511a477bc16e8496bfc4806515
Brad - DevOps Journey <34145011+devopsjourney1@users.noreply.github.com>
2023-05-08 19:41:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     hostname = socket.gethostname()
-    html = f"<h1>Hello World!</h1> Served from <b>{hostname}</b>"
+    html = f"<h1>Hello World!!</h1> Served from <b>{hostname}</b>"
     return html
 
 if __name__ == '__main__':
